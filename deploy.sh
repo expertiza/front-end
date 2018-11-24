045be8d65c07e4924391e0480c846fd054bd5460
Chirag Jain <csjain@ncsu.edu>
2018-11-24 10:53:02 -0500

    Update deploy script to include ansible and deploy env setup

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
+printf "********** Set the build type Prod/Beta *******************\n\n"
+export DEPLOY_ENV="$1"
+
 printf "********** Building Docker Image and pushing it ***********\n\n"
 . docker.sh
 printf "********** Done ***********\n\n"
 
-# printf "********** Pulling lastest image on dev server ************\n\n"
-# . ansible.sh
-# printf "********** Done ***********\n\n"
+printf "********** Deploying on ${DEPLOY_ENV} ************\n\n"
+. ansible.sh
+printf "********** Done ***********\n\n"
